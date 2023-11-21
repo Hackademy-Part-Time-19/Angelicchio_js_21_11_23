c34b9c308f41f203e748dab2be7db66e4d940edd
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2023-11-21 19:17:03 +0100

    primo esercizio

---
+++ Esercizio_1.js
@@ -0,0 +1,36 @@
+
+let listaProdotti = [
+    {
+        id: 1,
+        nome: "Iphone",
+        prezzo: 1000,
+    },
+    {
+        id: 2,
+        nome: "Mac",
+        prezzo: 3000,
+    },
+
+    {
+        id: 3,
+        nome: "Scrivania",
+        prezzo: 200,
+    },
+    {
+        id: 4,
+        nome: "Lampadari",
+        prezzo: 600,
+    },
+]
+
+/* Ordina per nome prodotto */
+listaProdotti.sort((a, b) => a.nome.localeCompare(b.nome));
+console.log(listaProdotti);
+
+/* Ordina per nome prezzo */
+listaProdotti.sort((a, b) => a.prezzo-b.prezzo);
+console.log(listaProdotti)
+
+/* Filtra  per prezzo > 250*/
+let listaFiltrata = listaProdotti.filter((a) => a.prezzo>250);
+console.log(listaFiltrata);
